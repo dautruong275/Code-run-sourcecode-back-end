e25f57efbac2ab2f8de54344ce899de1830f72e6
dautruongptit <dautruongptit@gmail.com>
2024-11-08 15:12:29 +0700

    Video YT17--BE27--FE?--Kafka

--- src/main/java/com/project/shopapp/controllers/CategoryController.java
+++ src/main/java/com/project/shopapp/controllers/CategoryController.java
@@ -1,16 +1,18 @@
 package com.project.shopapp.controllers;
 
 import com.project.shopapp.components.LocalizationUtils;
+//import com.project.shopapp.components.converters.CategoryMessageConverter;
+import com.project.shopapp.components.converters.CategoryMessageConverter;
 import com.project.shopapp.dtos.*;
 import com.project.shopapp.models.Category;
 import com.project.shopapp.responses.ResponseObject;
-import com.project.shopapp.responses.category.CategoryResponse;
 import com.project.shopapp.services.category.CategoryService;
 import com.project.shopapp.utils.MessageKeys;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -19,33 +21,40 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("${api.prefix}/categories")
 //@Validated
-//Dependency Injection
-@RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
     private final LocalizationUtils localizationUtils;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @PostMapping("")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     //Nếu tham số truyền vào là 1 object thì sao ? => Data Transfer Object = Request Object
-    public ResponseEntity<CategoryResponse> createCategory(
+    public ResponseEntity<ResponseObject> createCategory(
             @Valid @RequestBody CategoryDTO categoryDTO,
             BindingResult result) {
-        CategoryResponse categoryResponse = new CategoryResponse();
         if(result.hasErrors()) {
             List<String> errorMessages = result.getFieldErrors()
                     .stream()
                     .map(FieldError::getDefaultMessage)
                     .toList();
-            categoryResponse.setMessage(localizationUtils.getLocalizedMessage(MessageKeys.INSERT_CATEGORY_FAILED));
-            categoryResponse.setErrors(errorMessages);
-            return ResponseEntity.badRequest().body(categoryResponse);
+            return ResponseEntity.ok().body(ResponseObject.builder()
+                    .message(errorMessages.toString())
+                    .status(HttpStatus.BAD_REQUEST)
+                    .data(null)
+                    .build());
+
         }
         Category category = categoryService.createCategory(categoryDTO);
-        categoryResponse.setCategory(category);
-        return ResponseEntity.ok(categoryResponse);
+        this.kafkaTemplate.send("insert-a-category", category);//producer
+        this.kafkaTemplate.setMessageConverter(new CategoryMessageConverter());
+        return ResponseEntity.ok().body(ResponseObject.builder()
+                .message("Create category successfully")
+                .status(HttpStatus.OK)
+                .data(category)
+                .build());
     }
 
     //Hiện tất cả các categories
@@ -55,6 +64,13 @@ public class CategoryController {
             @RequestParam("limit")    int limit
     ) {
         List<Category> categories = categoryService.getAllCategories();
+        /*
+        this.kafkaTemplate.executeInTransaction(status -> {
+            categories.forEach(category -> kafkaTemplate.send("get-all-categories", category));
+            return null;
+        });
+         */
+        this.kafkaTemplate.send("get-all-categories", categories);
         return ResponseEntity.ok(ResponseObject.builder()
                 .message("Get list of categories successfully")
                 .status(HttpStatus.OK)
